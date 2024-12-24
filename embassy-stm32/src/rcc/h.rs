2ddb7ba91d1125fd9d11e3bbc45babb0ff14cb62
Rogan Morrow <rogan.o.morrow@gmail.com>
2024-12-24 22:16:51 +1100

    .

--- embassy-stm32/src/rcc/h.rs
+++ embassy-stm32/src/rcc/h.rs
@@ -416,9 +416,8 @@ pub(crate) unsafe fn init(config: Config) {
     while !RCC.cr().read().hsirdy() {}
 
     // Use the HSI clock as system clock during the actual clock setup
-    RCC.cfgr().modify(|w| w.set_sw(Sysclk::HSI));
-    while RCC.cfgr().read().sws() != Sysclk::HSI {}
-    loop {}
+    // RCC.cfgr().modify(|w| w.set_sw(Sysclk::HSI));
+    // while RCC.cfgr().read().sws() != Sysclk::HSI {}
 
     // Configure HSI
     let hsi = match config.hsi {
